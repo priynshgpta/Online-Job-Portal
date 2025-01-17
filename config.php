8363ec625dfa5a80a0b4a89f8401fd311f679467
Priyansh Gupta <71815344+priynshgpta@users.noreply.github.com>
2025-01-18 00:08:23 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /**
  */
-$host = "ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user  = "k89nihogyq5ofzae";
-$password =  "k3vnvf4yu01dmvxn";
-$database = "yovu5a3w3h9hfbjc";
+$host = "localhost";
+$user  = "root";
+$password =  "";
+$database = "jobportal";
 $db1 = new mysqli($host, $user, $password, $database);
 /*if($db1->connect_errno > 0){
     die('Unable to connect to database' . $db1->connect_error);
